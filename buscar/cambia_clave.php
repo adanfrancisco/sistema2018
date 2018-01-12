4fa99d894401e2cb9d9ce54b169ab71f4c66b6bc
adanfrancisco <adanfrancisco@gmail.com>
2018-01-11 21:05:56 -0300

    PUEDO DETECTAR QUE PROFE TIENE USUARIO

--- buscar/cambia_clave.php
+++ buscar/cambia_clave.php
@@ -7,6 +7,20 @@ $usuario=$_SESSION['usuario_id'];
 
 include('../acceso_db.php');
 
-echo $ddni.'-'.$clave_nueva;
+//echo $ddni.'-'.$clave_nueva;
+$sql="select * from usuarios 
+inner join profesores 
+on usuarios.usuario_id=profesores.usuario_id 
+where dni=$ddni";
+if ($resultado = $con->query($sql)){
 
+	if($resultado->num_rows > 0){
+		echo 'Tiene usuario';
+	}else{
+		echo 'esa persona no tiene usuario';
+	}
+}else{
+	echo 'esa persona existe, pero no tiene usuario';
+}
+//echo $sql;
  ?>
