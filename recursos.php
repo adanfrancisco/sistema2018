356c333095d7e076bf82336c938a64bda2991196
adanfrancisco <adanfrancisco@gmail.com>
2018-01-11 21:15:44 -0300

    actualize el archivo de RECURSOS

--- recursos.php
+++ recursos.php
@@ -9,3 +9,17 @@ else
    $('#content').hide();
 
    </script>
+
+<?php
+   if ($resultado = $con->query($sql)){
+
+	if($resultado->num_rows > 0){
+		if ($resultado = $con->query($sql)){    
+            while ($fila = $resultado->fetch_array(MYSQLI_BOTH)) {
+
+            echo'<OPTION VALUE="'.$fila['fecha'].'">'.$fila['fecha'].'</OPTION>    ';
+                echo $fila['fecha'];
+            }
+            echo '</select>';
+    }
+?>
