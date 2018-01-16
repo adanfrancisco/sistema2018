e4a5395999dbe5ea6dc2a76e434ec0a84f6e0ed5
Adan <adanfrancisco@gmail.com>
2018-01-16 15:18:31 -0300

    Dtpicker

--- recursos.js
+++ recursos.js
@@ -10,3 +10,15 @@ function crearElemento(elemento, identificador, clase, texto, ruta, valor) {
 btn = crearElemento('button','num_parrafos','agrega tus clases','Numero parrafos','__','__');
 body = document.body;
 body.appendChild(btn);
+
+Dtpicker con fecha de hoy
+
+$(document).ready(function() {
+    $("#datepicker").datepicker({
+	dateFormat: 'dd/mm/yy',
+    }).datepicker("setDate", new Date());
+});
+<link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script> 
+<input type="text" id="datepicker">
