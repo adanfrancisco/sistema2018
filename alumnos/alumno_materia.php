659a438567f0bf677c3498ffe7d1763022f4c7c8
adanfrancisco <adanfrancisco@gmail.com>
2018-02-04 18:34:27 -0300

    requisitos de inscripcion a DESARROLLAR

---
+++ alumnos/alumno_materia.php
@@ -0,0 +1,11 @@
+/*
+lo ideal seria que el alumno 
+- ingrese en el sistema con su dni
+- cambie su clave
+- se inscriba en las materias que desea.. 
+
+El sistema deberi:
+- comprobar si tiene aprobada las cursadas
+- personalizar la inscripcion en funcion de los finales aprobados
+- permitir el registro del alumno en examenes finales (comprobando correlativas)
+- 
