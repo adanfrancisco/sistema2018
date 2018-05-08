f01a21c7d1aae6f8630df224901b6c87c7047b0c
adanfrancisco <adanfrancisco@gmail.com>
2018-05-08 01:18:53 -0300

    mejorado, ahora aparece la materia

--- profe_materia/disocia_profe_suplente.php
+++ profe_materia/disocia_profe_suplente.php
@@ -1,5 +1,5 @@
 <?php
-echo 'si';
+//echo 'si';
     session_start();
     include('../acceso_db.php'); // incluímos los datos de conexión a la BD
 
@@ -17,12 +17,12 @@ inner join mat_pro on mat_pro.id_matpro=mat_pro_novedades.matpro_id
 inner join curso_lectivo on mat_pro.curso_lectivo_id=curso_lectivo.idcursolectivo
 inner join curso on curso.idcurso=curso_lectivo.curso
 inner join carrera on curso.carrera_id=carrera.id_carrera
-#inner join materia on mat_pro.materia=materia.id_materia  
+inner join materia on mat_pro.materia=materia.id_materia  
 inner join profesores on profesores.dni=mat_pro.profesor
-inner join profesores on profesores.dni=mat_pro.reemplazo
+#inner join profesores on profesores.dni=mat_pro.reemplazo
 where reemplazo <>0 and activo=1 
 ORDER BY materia";
-//echo $consultacurso;
+echo $consultacurso;
 ?>
 <select name="" id="">
 <?php   
@@ -30,7 +30,7 @@ ORDER BY materia";
          {
             while ($fila = $resultado->fetch_array(MYSQLI_BOTH)) 
             {
-                $options='<option value="'.$fila['materia'].'"> '.$fila['materia'].'</option>';
+                $options='<option value="'.$fila['materia'].'"> '.$fila['materia_nombre'].'</option>';
                 echo $options;  
             }
         }      
