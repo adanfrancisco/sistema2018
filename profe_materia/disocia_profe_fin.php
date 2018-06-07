e9bd9bd82bb5f18e9baa80bcda723fb779b40694
adanfrancisco <adanfrancisco@gmail.com>
2018-06-07 11:23:23 -0300

    falta cambiar el estado

--- profe_materia/disocia_profe_fin.php
+++ profe_materia/disocia_profe_fin.php
@@ -14,12 +14,14 @@ $curso=$_POST['curso'];
 //verifico si existe el profe suplido
 $suplido="select materia,curso_lectivo_id, profesor, reemplazo , activo
 from mat_pro where profesor =".$suplido." and activo=0 and curso_lectivo_id=".$curso." and materia=".$materia;
-echo $suplido;
+//echo $suplido;
 $resultado = $con->query($suplido);
-echo '<br>'.$resultado->num_rows;
-
+echo '<br> Se encontro: '.$resultado->num_rows. 'coincidencia';
+if($resultado->num_rows>0){
 //cambio el estado de Activo a no y reversa de ambos
+echo '<br>cambiamos el estado de activo a no y activamos el anterior';
 
+}
 
 ?>
 
